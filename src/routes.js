f0503a62df2d5fc84804b8db206f5f8b2d914cc2
Carlos Daniel <carlosdandeveloper@gmail.com>
2026-06-10 20:03:48 -0300

    feat: adicionar métodos para extrair parâmetros da url

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import { buildRoutePath } from "./utils/build-route-path.js";
 import { TaskController } from "./controllers/task-controller.js";
 
 const taskController = new TaskController();
@@ -5,12 +6,12 @@ const taskController = new TaskController();
 export const routes = [
   {
     method: "GET",
-    path: "/tasks",
+    path: buildRoutePath("/tasks"),
     handler: taskController.getAll,
   },
   {
     method: "POST",
-    path: "/tasks",
+    path: buildRoutePath("/tasks"),
     handler: taskController.create,
   },
 ];
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from "node:http";
 
-import { routes } from "./routes.js";
 import { json } from "./middlewares/json.js";
+import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -9,10 +10,17 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   const route = routes.find((route) => {
-    return route.method === method && route.path === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = url.match(route.path);
+
+    const { query, params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
     return route.handler(req, res);
   }
 
